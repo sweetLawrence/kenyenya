f45de9bf83100cea6ce357b7d852b7c36dc82ea8
Lawrence Tsungu <tsungulawrence@gmail.com>
2024-01-02 13:56:46 +0300

    code

--- src/Pages/Experiment/Components/SignupLogin.js
+++ src/Pages/Experiment/Components/SignupLogin.js
@@ -47,7 +47,7 @@ function SignupLogin() {
             } else if (!login.kcseIndex) {
                 toast.error('Please fill in your KCSE Index Number/Year ');
             } else {
-                const response = await axios.post('https://67cc-154-159-237-203.ngrok-free.app/api/login', login);
+                const response = await axios.post('https://d8dc-154-159-237-69.ngrok-free.app/api/login', login);
                 const accessToken = response.data;
                 localStorage.setItem('accessToken', accessToken);
                 // alert(accessToken)
