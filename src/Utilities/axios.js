7418156b0e717778c8ef340a1e85fa44004d19b2
Lawrence Tsungu <tsungulawrence@gmail.com>
2024-01-09 20:30:18 +0300

    code

--- src/Utilities/axios.js
+++ src/Utilities/axios.js
@@ -1,6 +1,5 @@
 let basePath = window.location.href.includes("localhost")
     ? "http://localhost:3001"
-    : "https://8e08-154-159-237-179.ngrok-free.app";
+    : "https://ae22-154-159-237-97.ngrok-free.app";
 
 export default basePath;
-
